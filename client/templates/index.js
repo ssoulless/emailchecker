f3042fbb49c2f878b7b2f590743cdfbf5bad2cc6
Miller Stiven <stiven-0611@hotmail.com>
2015-09-16 16:04:04 -0500

    added support for the spinner, Added basic impletentation for the file reader

--- client/templates/index.js
+++ client/templates/index.js
@@ -6,5 +6,10 @@ Template.index.events({
 				console.log(err.reason);
 			});
 		});
+	},
+	'click .analyze': function(e, t) {
+		var emails = Assets.getText('seeds/'+Uploads.find({sort: {createdAt: -1}, limit: 1}).fetch()[0].name)
+		//TODO: leer el archivo en un parser de texto plano
+		//TODO: crear funcionalidades
 	}
 });
