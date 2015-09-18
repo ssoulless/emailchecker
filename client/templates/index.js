12602a98531352981d02209cb440cf011a23c571
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-09-18 00:00:10 -0500

    Now uploading file from UI

--- client/templates/index.js
+++ client/templates/index.js
@@ -1,5 +1,5 @@
 Template.index.events({
-	/*'change .fileInput': function (e, t) {
+	'change .fileInput': function (e, t) {
 		FS.Utility.eachFile(e, function(file){
 			var fileObj = new FS.File(file)
 			Uploads.insert(fileObj, function(err){
@@ -7,9 +7,9 @@ Template.index.events({
 			});
 		});
 	},
-	'click .analyze': function(e, t) {
-		var emails = Assets.getText('seeds/'+Uploads.find({sort: {createdAt: -1}, limit: 1}).fetch()[0].name)
-		//TODO: leer el archivo en un parser de texto plano
-		//TODO: crear funcionalidades
-	}*/
+	// 'click .analyze': function(e, t) {
+	// 	var emails = Assets.getText('seeds/'+Uploads.find({sort: {createdAt: -1}, limit: 1}).fetch()[0].name)
+	// 	//TODO: leer el archivo en un parser de texto plano
+	// 	//TODO: crear funcionalidades
+	// }
 });
