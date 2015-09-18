08c651bf212ab16b9d1bcc84173f904d9d3c98f2
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-09-18 01:02:49 -0500

    Added flash messages

--- client/templates/index.js
+++ client/templates/index.js
@@ -3,16 +3,22 @@ Template.index.events({
 		FS.Utility.eachFile(e, function(file){
 			var fileObj = new FS.File(file)
 			Session.set('currentUpload', true);
-			Uploads.insert(fileObj, function(err){
+			Uploads.insert(fileObj, function(err, result){
+				if (err) {
+				Messages.send('alert', err.reason);
 				console.log(err.reason);
+				return;
+				}
+				Messages.send('info', 'File uploaded')
 			});
 		});
 	},
-	// 'click .analyze': function(e, t) {
-	// 	var emails = Assets.getText('seeds/'+Uploads.find({sort: {createdAt: -1}, limit: 1}).fetch()[0].name)
-	// 	//TODO: leer el archivo en un parser de texto plano
-	// 	//TODO: crear funcionalidades
-	// }
+	'click .analyze': function(e, t) {
+		Messages.send('info','Ups this is not ready yet')
+		//var emails = Assets.getText('seeds/'+Uploads.find({sort: {createdAt: -1}, limit: 1}).fetch()[0].name)
+		//TODO: leer el archivo en un parser de texto plano
+		//TODO: crear funcionalidades
+	}
 });
 
 Template.index.helpers({
