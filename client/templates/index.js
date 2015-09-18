66fa6a43fdf61b9bf5c749bee28984a7ed62bb61
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-09-18 00:22:42 -0500

    list the last uploaded file, added session variable for identify if it is current upload otherwise hide last upload

--- client/templates/index.js
+++ client/templates/index.js
@@ -2,6 +2,7 @@ Template.index.events({
 	'change .fileInput': function (e, t) {
 		FS.Utility.eachFile(e, function(file){
 			var fileObj = new FS.File(file)
+			Session.set('currentUpload', true);
 			Uploads.insert(fileObj, function(err){
 				console.log(err.reason);
 			});
@@ -12,4 +13,14 @@ Template.index.events({
 	// 	//TODO: leer el archivo en un parser de texto plano
 	// 	//TODO: crear funcionalidades
 	// }
+});
+
+Template.index.helpers({
+	upload: function(){
+		if (Session.get('currentUpload')){
+				return Uploads.find({},{sort: {uploadedAt: -1}, limit: 1});
+		}else{
+			return [];
+		}
+	}
 });
