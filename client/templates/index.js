e7abdea169670c0b71cc82dfcedec1596a545a95
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-09-17 23:21:18 -0500

    Fixed syntax errors and bugs.

--- client/templates/index.js
+++ client/templates/index.js
@@ -1,5 +1,5 @@
 Template.index.events({
-	'change .fileInput': function (e, t) {
+	/*'change .fileInput': function (e, t) {
 		FS.Utility.eachFile(e, function(file){
 			var fileObj = new FS.File(file)
 			Uploads.insert(fileObj, function(err){
@@ -11,5 +11,5 @@ Template.index.events({
 		var emails = Assets.getText('seeds/'+Uploads.find({sort: {createdAt: -1}, limit: 1}).fetch()[0].name)
 		//TODO: leer el archivo en un parser de texto plano
 		//TODO: crear funcionalidades
-	}
+	}*/
 });
